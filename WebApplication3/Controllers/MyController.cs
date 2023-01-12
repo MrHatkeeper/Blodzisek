a5f222d47202057a29b79e6b9771f3b01a22a8f3
MrHatkeeper <58586406+MrHatkeeper@users.noreply.github.com>
2023-01-12 21:32:06 +0100

    Funguje to

--- WebApplication3/Controllers/MyController.cs
+++ WebApplication3/Controllers/MyController.cs
@@ -5,21 +5,37 @@ namespace WebApplication3.Controllers;
 public class MyController : Controller
 {
     // GET
+    private Posts _posts = new Posts();
 
     public IActionResult Home()
     {
-        Posts posts = new Posts();
-        List<OnePost> data = posts.ReadPosts();
-        ViewData["posts"] = posts;
+
+        List<OnePost> data = _posts.ReadPosts();
+        ViewData["posts"] = _posts;
         ViewData["data"] = data;
         return View();
     }
 
     [HttpPost]
-    public IActionResult AddPost(string header, string body)
+    public Task<IActionResult> AddPost(string header, string body)
     {
-        ViewBag.
-        return View();
+        _posts.AddPost(header, body);
+        return Task.FromResult<IActionResult>(RedirectToAction("Home"));
     }
-    
+
+
+    [HttpPost]
+    public Task<IActionResult> Remove(int id)
+    {
+        _posts.RemovePost(id);
+        return Task.FromResult<IActionResult>(RedirectToAction("Home"));
+    }
+
+    [HttpPost]
+    public Task<IActionResult> Edit(int id, string header, string body)
+    {
+        _posts.EditPost(id, header, body);
+        return Task.FromResult<IActionResult>(RedirectToAction("Home"));
+    }
+
 }
--- WebApplication3/Controllers/Posts.cs
+++ WebApplication3/Controllers/Posts.cs
@@ -4,7 +4,7 @@ namespace WebApplication3.Controllers;
 
 public class Posts
 {
-    private readonly string path = "bin/Debug/net7.0/idkPosts.csv";
+    private const string Path = "bin/Debug/net7.0/idkPosts.csv";
 
     public void EditPost(int id, string newHeader, string newBody)
     {
@@ -13,31 +13,39 @@ public class Posts
     
     public void RemovePost(int id)
     {
+        var lines = File.ReadAllLines(Path).Where(arg => !string.IsNullOrWhiteSpace(arg));
+        File.WriteAllLines(Path, lines);
+        
+        
         List<string> records =
             File.ReadAllLines(
-                path).ToList();
-        records.RemoveAt(id - 1);
-        File.WriteAllLines(path,
+                Path).ToList();
+        records.RemoveAt(id);
+        File.WriteAllLines(Path,
             records.ToArray());
     }
 
     public void AddPost(string postHeader, string postBody)
     {
+        var lines = File.ReadAllLines(Path).Where(arg => !string.IsNullOrWhiteSpace(arg));
+        File.WriteAllLines(Path, lines);
+        
         int lineCount = File
-            .ReadLines(path)
+            .ReadLines(Path)
             .Count();
-        string content = "\n" + lineCount + ";" + postHeader + ";" + postBody;
-        File.AppendAllText(path,
+        string content = "\n" + (lineCount + 1) + ";" + postHeader + ";" + postBody;
+        File.AppendAllText(Path,
             content);
     }
 
     public List<OnePost> ReadPosts()
     {
         List<OnePost> posts = new List<OnePost>();
-        var input = File.ReadLines(path);
+        var input = File.ReadLines(Path);
 
         foreach (var post in input)
         {
+            if (post.Length <= 0) continue;
             var tempPost = post.Split(";");
             posts.Add(new OnePost(int.Parse(tempPost[0]), tempPost[1], tempPost[2]));
         }
